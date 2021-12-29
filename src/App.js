06bfdc62cc7397689e0cec9d3218fd95abad4992
ZjeniiaSt <zeniiastupjak@gmail.com>
2021-12-29 10:57:09 +0300

    localSt

--- src/App.js
+++ src/App.js
@@ -48,6 +48,20 @@ class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const visibleContacts = this.getFindContact();
     return (
